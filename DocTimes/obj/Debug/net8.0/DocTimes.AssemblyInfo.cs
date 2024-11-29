70659ba9260bfaed1e8fde8cfec252558c8f9d29
rimon19 <mominur.hossain@outlook.com>
2024-11-29 23:24:09 +0600

    update

--- DocTimes/obj/Debug/net8.0/DocTimes.AssemblyInfo.cs
+++ DocTimes/obj/Debug/net8.0/DocTimes.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DocTimes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+163122a2aa4d430cc8a6a2fc981eb2bb170e41be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+306e9295a98b5a8ee8c95b1e5d85ab29d92c59bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DocTimes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DocTimes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
